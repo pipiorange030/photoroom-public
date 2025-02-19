30cdf25279f84a8f93c8b0f3e935932f9ba5321b
pipiorange030 <147233508+pipiorange030@users.noreply.github.com>
2025-02-19 16:12:27 +0800

    Update removed.js

--- removed.js
+++ removed.js
@@ -1,38 +1,12 @@
 const css = `
-#fake_avivid_waterfall_webpush1,
-.a__gs_cy,
-.fake_avivid_waterfall_webpush_active_left,
-.normal-top:nth-of-type(1),
-.ad-area-close,
-.ad-area-close ~ div,
-.reader-cartoon-chapter iframe,
-.reader-book-read-wraper article div:first-child,
-.index-banner,
-.manga-list:first-of-type,
-.manga-list-2:nth-of-type(1),
-.manga-list-title:nth-of-type(1),
-.index-marquee,
-.ifpopup_content,
-.ifpopup_div,
-.row.stui-pannel,
-.blocked_hint_msg_pc,
-#index-marquee-block,
-.app_download,
-.reader-home-swiper,
-.reader-home-title-fixed,
-#temp_block_01 + div,
-#temp_block_02 + div,
-#temp_block_03 + div,
-#temp_block_04 + div,
-.reader-zone-list-view.list.no-hairlines + div,
-iframe,
-.guess_mh_list + div {
+/* 隐藏 ad-top-info 元素 */
+div.ad-top-info {
     display: none !important;
 }
 
-.ad-area {
-    opacity: 0 !important;
-    height: 0px !important;
+/* 隐藏 two-ad-area 元素 */
+div.two-ad-area {
+    display: none !important;
 }
 `;
 

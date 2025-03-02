24ba0736a9dbb3571f1bb1c17f352f80c5e85275
pipiorange030 <147233508+pipiorange030@users.noreply.github.com>
2025-03-02 22:27:26 +0800

    Delete ad.js

--- ad.js
+++
@@ -1,5 +0,0 @@
-let url = 'https://github.com/pipiorange030/photoroom/blob/main/newcss.css';
-let originalResponse = $response.body;
-let newCssLink = `<link rel="stylesheet" type="text/css" href="${url}">`;
-let newBody = originalResponse.replace(/<\/head>/i, `${newCssLink}</head>`);
-$done({ body: newBody });
